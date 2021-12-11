e4c50be042cc054caa2ea1fdb026e8525ecbb12a
Audi0s1ave <nikhilx7y@gmail.com>
2021-12-12 02:25:50 +0530

    next
    Signed-off-by: Audi0s1ave <nikhilx7y@gmail.com>

--- src/main/java/com/Tranzactions/controllers/Controller.java
+++ src/main/java/com/Tranzactions/controllers/Controller.java
@@ -1,2 +1,14 @@
-package com.Tranzactions.controllers;public class Controller {
+package com.Tranzactions.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
 }
